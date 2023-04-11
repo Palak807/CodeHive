0afc1f0623e93b433832037aa58b5147c530d89b
Akshat Dixit <78369837+Dixzen@users.noreply.github.com>
2023-04-11 14:42:39 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import "./global.css"
+
+import { Provider } from 'react-redux';
+import { reducers } from "./redux/reducers"
+import thunk from "redux-thunk"
+import {BrowserRouter} from "react-router-dom"
+import { createStore, applyMiddleware, compose} from "redux"
+import {GoogleOAuthProvider} from "@react-oauth/google"
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, {}, composeEnhancers(applyMiddleware(thunk)))
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+      <GoogleOAuthProvider
+                clientId={'576509317662-1uhgji0ut3lk0vs8rvlk5l1ah3gpha9u.apps.googleusercontent.com'}>
+       <App />
+       </GoogleOAuthProvider>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
+
+
+reportWebVitals();
