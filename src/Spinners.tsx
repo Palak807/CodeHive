df85720b09f62c83eb0283f3f3ecfdad878bb555
Palak807 <88302656+Palak807@users.noreply.github.com>
2023-04-11 14:29:19 +0530

    Add files via upload

---
+++ src/Spinners.tsx
@@ -0,0 +1,13 @@
+
+import PropagateLoader from "react-spinners/PropagateLoader";
+
+const Spinners = () => {
+   
+  return (
+    <div className="sweet-loading">
+      <PropagateLoader color="#f1356d" size={15}/>
+    </div>
+  )
+}
+
+export default Spinners
