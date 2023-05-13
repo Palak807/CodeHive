a07052be4bd48ec51ad6f686ceb89d87909bc6c8
parijatkasbekar <70934720+parijatkasbekar@users.noreply.github.com>
2023-05-14 01:44:58 +0530

    Update index.jsx

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,11 +1,11 @@
-import { Box, useMediaQuery } from "@mui/material";
-import { useSelector} from "react-redux";
-import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
 import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
 import AdvertWidget from "scenes/widgets/AdvertWidget";
 import FriendListWidget from "scenes/widgets/FriendListWidget";
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector} from "react-redux";
+import Navbar from "scenes/navbar";
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
